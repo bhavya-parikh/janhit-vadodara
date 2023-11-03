c3b72769fb23780029def9b8d618d9d3c7406624
Keyur2803 <keyurpansuriya0000@gmail.com>
2023-11-03 18:01:48 +0530

    Added Login/Signup UI

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,28 @@
-import { Navigate, Route, Routes } from "react-router-dom";
 import "./App.css";
-import Auth from "./components/auth";
+import Profile from "./components/Profile/Profile";
+import Login from "./components/Login/Login";
+import Register from "./components/Register/Register";
+import OTPVerification from "./components/Register/OTPVerification";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { useState } from "react";
+import { ToastContainer } from "react-toastify";
+import Layout from "./components/Layout/Layout"
 function App() {
+  const [userstate, setUserState] = useState({});
   return (
-    <div className="app">
-      <Routes>
-        <Route path="/auth" element={<Auth />} />
-      </Routes>
+    <div className="App">
+        <Routes>
+          <Route path="/" element={<Layout />}></Route>
+          <Route 
+            path="/otpverification" 
+              element={<OTPVerification />}></Route>
+          <Route
+            path="/login"
+            element={<Login setUserState={setUserState} />}
+          ></Route>
+          <Route path="/signup" element={<Register />}></Route>
+        </Routes>
+      
     </div>
   );
 }
