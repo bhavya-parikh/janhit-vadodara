62eade9a09d51a9014b462ad11e31b6a5fbab6ae
Rutu patel <rutupatel053@gmail.com>
2023-11-19 00:40:13 +0530

    Revert "changes done"
    This reverts commit dddd8dc6fd9365c46c078d9d03361de532d839bf.

--- server/index.js
+++ server/index.js
@@ -16,16 +16,10 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/uploads", express.static("uploads"));
 const cors = require("cors");
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3001;
 const db = require("./db/user");
 
-app.use(
-  cors({
-    origin: ["http://localhost:3000"],
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true,
-  })
-);
+app.use(cors());
 app.use(cookieParser());
 
 const apiRoutes = require("./routes/api");
@@ -34,5 +28,5 @@ app.get("/", (req, res) => {
   res.send("Hey this is my API running 🥳");
 });
 app.listen(PORT, () => {
-  console.log("Server running on port ${PORT}");
+  console.log(`Server running on port ${PORT}`);
 });
