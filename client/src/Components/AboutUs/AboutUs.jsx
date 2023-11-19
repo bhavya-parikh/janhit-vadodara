9c7f43c126a4ef14d631003d70cf8520a3999bad
Rutu patel <rutupatel053@gmail.com>
2023-11-19 22:17:39 +0530

    fixed authnavbar hamburger and updated AdminLogin

--- client/src/Components/AboutUs/AboutUs.jsx
+++ client/src/Components/AboutUs/AboutUs.jsx
@@ -1,7 +1,7 @@
 // AboutUsPage.jsx
 
-import React from 'react';
-import './AboutUs.css';
+import React from "react";
+import "./AboutUs.css";
 
 const AboutUsPage = () => {
   return (
@@ -10,21 +10,29 @@ const AboutUsPage = () => {
 
       <div className="content-section">
         <p className="paragraph">
-          Welcome to the Vadodara Municipal Corporation's online portal. We are dedicated to providing efficient services and addressing civic issues to enhance the quality of life for the residents of Vadodara.
+          Welcome to the Vadodara Municipal Corporation's online portal. We are
+          dedicated to providing efficient services and addressing civic issues
+          to enhance the quality of life for the residents of Vadodara.
         </p>
       </div>
 
       <div className="content-section">
         <h2 className="section-subtitle">Our Mission</h2>
         <p className="paragraph">
-          At Vadodara Municipal Corporation, our mission is to create a cleaner, safer, and more sustainable city by addressing and resolving civic issues effectively. We aim to provide transparent, accountable, and citizen-centric services to meet the diverse needs of our community.
+          At Vadodara Municipal Corporation, our mission is to create a cleaner,
+          safer, and more sustainable city by addressing and resolving civic
+          issues effectively. We aim to provide transparent, accountable, and
+          citizen-centric services to meet the diverse needs of our community.
         </p>
       </div>
 
       <div className="content-section">
         <h2 className="section-subtitle">Services We Offer</h2>
         <ul className="service-list">
-          <li>Register complaints related to electricity, waterlogging, and other civic problems.</li>
+          <li>
+            Register complaints related to electricity, waterlogging, and other
+            civic problems.
+          </li>
           <li>Track the status of your complaints in real-time.</li>
           <li>Access government services efficiently.</li>
           {/* Add more services as needed */}
@@ -34,15 +42,18 @@ const AboutUsPage = () => {
       <div className="content-section">
         <h2 className="section-subtitle">Contact Information</h2>
         <p className="paragraph">
-          Address: [Add the address here]<br />
-          Phone: [Add the contact number here]<br />
+          Address: [Add the address here]
+          <br />
+          Phone: [Add the contact number here]
+          <br />
           Email: [Add the email address here]
         </p>
       </div>
 
       <div className="content-section">
         <p className="paragraph">
-          Thank you for choosing Vadodara Municipal Corporation. We are committed to serving you and making Vadodara a better place for all.
+          Thank you for choosing Vadodara Municipal Corporation. We are
+          committed to serving you and making Vadodara a better place for all.
         </p>
       </div>
     </div>
---
+++ client/src/Components/Commissioner/RemoveDeptHead.jsx
@@ -0,0 +1,81 @@
+import React, { useState } from "react";
+import { Form, Input, Button, Select } from "antd";
+import axios from "axios";
+import { ToastContainer, toast } from "react-toastify";
+
+export const RemoveDeptHead = () => {
+  const [form] = Form.useForm();
+  const [formErrors, setFormErrors] = useState({});
+
+  const onFinish = async (values) => {
+    try {
+      const response = await axios.post(
+        `${process.env.REACT_APP_VERCEL_ENV_BASEURL}/api/removeFieldStaff`,
+        values,
+        {
+          withCredentials: true,
+        }
+      );
+      console.log("DeptHead removed successfully:", response.data);
+      toast("DeptHead Removed successfully");
+    } catch (error) {
+      console.error("Error removing DeptHead:", error.message);
+    }
+  };
+
+  const validateForm = (values) => {
+    const errors = {};
+
+    if (!values.username) {
+      errors.username = "Username is required";
+    }
+
+    return errors;
+  };
+
+  return (
+    <div className="bg-white border-2 border-gray-300 shadow-md rounded-md p-4 mt-4 md:mt-24 max-w-md mx-auto">
+      <Form
+        form={form}
+        name="RemoveDeptHead"
+        onFinish={onFinish}
+        onValuesChange={() => {
+          setFormErrors({});
+        }}
+        initialValues={{
+          remember: true,
+        }}
+      >
+        <h1 className="text-3xl font-bold mb-4 text-black text-center md:text-left">
+          Remove DepartMent Head
+        </h1>
+        <Form.Item
+          label="Username"
+          name="username"
+          rules={[
+            {
+              required: true,
+              message: "Please enter your username!",
+            },
+          ]}
+        >
+          <Input
+            placeholder="Username"
+            className="rounded-lg border border-gray-300 p-2 mb-2 w-full"
+          />
+        </Form.Item>
+        <div className=" flex justify-center  md:h-fit md:col-start-2 md:col-span-1 md:mr-4 flex justify-center md:justify-end md:ml-2">
+          <Form.Item>
+            <Button
+              type="primary"
+              htmlType="submit"
+              className="bg-blue-700 mr-16 w-76 h-12 text-white font-bold rounded-full text-xl  transition-all duration-300 transform hover:bg-blue-500 focus:outline-none focus:shadow-outline-blue active:bg-blue-800"
+            >
+              Submit
+            </Button>
+          </Form.Item>
+        </div>
+      </Form>
+    </div>
+  );
+};
