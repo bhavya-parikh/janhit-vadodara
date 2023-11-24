3a808f39202676134412f9a60abffccdedc0dc0b
Keyur2803 <keyurpansuriya0000@gmail.com>
2023-11-24 22:33:07 +0530

    reopen reason added

--- client/src/Components/MyComplaints/MyComplaints.jsx
+++ client/src/Components/MyComplaints/MyComplaints.jsx
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
-import { Navigate, useParams } from "react-router-dom";
 import { toast } from "react-toastify";
 import { Steps, message } from "antd";
+import { useParams } from "react-router-dom";
 import { SmileOutlined } from "@ant-design/icons";
 
 export default function MyComplaints() {
@@ -11,16 +11,17 @@ export default function MyComplaints() {
   const { complaintId, issueDescription } = useParams();
   const [complaints, setComplaints] = useState([]);
   const navigate = useNavigate();
+const [currentComplaintId,setCurrentComplaintId] = useState("");
+  const [reopenModalVisible, setReopenModalVisible] = useState(false);
+  const [reopenReason, setReopenReason] = useState("");
 
-  const updateComplaintStatus = (complaintId, newStatus) => {
+  const updateComplaintStatus = (complaintId, complaintStatus, reason) => {
     axios
-      .post(
-        `${process.env.REACT_APP_VERCEL_ENV_BASEURL}/api/updateComplaintStatus`,
-        {
-          complaintId,
-          newStatus,
-        }
-      )
+      .post(`${process.env.REACT_APP_VERCEL_ENV_BASEURL}/api/user/updateComplaintStatus`, {
+        complaintId,
+        complaintStatus,
+        reason,
+      })
       .then((response) => {
         console.log("Status Updated Successfully!");
         message.success("Status Updated Successfully!");
@@ -29,7 +30,7 @@ export default function MyComplaints() {
         setComplaints((prevComplaints) =>
           prevComplaints.map((complaint) =>
             complaint._id === complaintId
-              ? { ...complaint, complaintStatus: newStatus }
+              ? { ...complaint, complaintStatus: complaintStatus }
               : complaint
           )
         );
@@ -53,7 +54,6 @@ export default function MyComplaints() {
         }
       )
       .then((response) => {
-        console.log(response);
         setComplaints(response.data);
       })
       .catch((error) => {
@@ -86,6 +86,7 @@ export default function MyComplaints() {
         return "";
     }
   };
+
   const items2 = [
     {
       title: "Complaint Registered",
@@ -132,6 +133,25 @@ export default function MyComplaints() {
     },
   ];
 
+  const reopenHandler = (cid) => {
+    setCurrentComplaintId(cid);
+    openReopenModal();
+  }
+
+  const openReopenModal = () => {
+    setReopenModalVisible(true);
+  };
+
+  const closeReopenModal = () => {
+    setReopenModalVisible(false);
+    setReopenReason("");
+  };
+
+  const handleReopen = () => {
+    updateComplaintStatus(currentComplaintId, "Escalated", reopenReason);
+    closeReopenModal();
+  };
+
   return (
     <div className="container mx-auto p-8">
       <h1 className="text-4xl font-bold mb-6 text-black text-decoration-line: underline text-center">
@@ -140,16 +160,11 @@ export default function MyComplaints() {
 
       {complaints.length === 0 ? (
         <div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-          <p className="text-6xl font-bold text-gray-600">
-            No complaints found.
-          </p>
+          <p className="text-6xl font-bold text-gray-600">No complaints found.</p>
         </div>
       ) : (
         complaints.map((complaint, index) => (
-          <div
-            key={complaint._id}
-            className="bg-white p-6 rounded-lg shadow-md mb-4"
-          >
+          <div key={complaint._id} className="bg-white p-6 rounded-lg shadow-md mb-4">
             <div className="grid grid-cols-2 gap-4">
               <div className="mb-4">
                 <label
@@ -228,42 +243,51 @@ export default function MyComplaints() {
             </>
             {(complaint.complaintStatus === "Completed" ||
               complaint.complaintStatus === "Disposed") && (
-              <button
-                className="bg-blue-500 text-white py-2 px-4 rounded"
-                onClick={() => {
-                  updateComplaintStatus(complaint._id, "Escalated");
-                  axios
-                    .post(
-                      `${process.env.REACT_APP_VERCEL_ENV_BASEURL}/api/fetchComplaintsUser`,
-                      {},
-                      {
-                        withCredentials: true,
-                      }
-                    )
-                    .then((response) => {
-                      setComplaints(response.data);
-                    })
-                    .catch((error) => {
-                      toast(error.response.data.message);
-                    });
-                }}
-              >
-                Reopen
-              </button>
-            )}
-            {complaint.complaintStatus === "Completed" && (
-              <button
-                className="bg-green-500 text-white py-2 px-4 rounded"
-                onClick={() => {
-                  navigate("/Review");
-                }}
-              >
-                Write Review
-              </button>
-            )}
+                <button
+                  className="bg-blue-500 text-white py-2 px-4 rounded"
+                  onClick={() => reopenHandler(complaint._id)}
+                >
+                  Reopen
+                </button>
+              )}
+              {complaint.complaintStatus === "Completed" && (
+                <button
+                  className="bg-green-500 text-white py-2 px-4 rounded"
+                  onClick={() => {
+                    navigate("/Review");
+                  }}
+                >
+                  Write Review
+                </button>
+              )}
+            </div>
+          ))
+        )}
+  
+        {/* Reopen Modal */}
+        {reopenModalVisible && (
+          <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-gray-800 bg-opacity-50">
+            <div className="bg-white p-8 rounded-md">
+              <h2 className="text-2xl font-bold mb-4">Enter reason for reopening</h2>
+              <textarea
+                rows="4"
+                className="w-full mb-4 p-2 border border-gray-300 rounded"
+                placeholder="Enter reason..."
+                value={reopenReason}
+                onChange={(e) => setReopenReason(e.target.value)}
+              />
+              <div className="flex justify-end">
+                <button className="bg-blue-500 text-white py-2 px-4 rounded mr-2" onClick={handleReopen}>
+                  Reopen
+                </button>
+                <button className="bg-gray-500 text-white py-2 px-4 rounded" onClick={closeReopenModal}>
+                  Cancel
+                </button>
+              </div>
+            </div>
           </div>
-        ))
-      )}
-    </div>
-  );
-}
+        )}
+      </div>
+    );
+  }
+  
